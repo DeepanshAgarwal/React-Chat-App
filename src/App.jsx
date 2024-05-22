85aeef35d080bbea58be29648b59904ce3bb6e84
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2024-05-22 21:36:30 +0530

    make basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-const App = () => {
-    return <div className="">App</div>;
-};
+import Chat from "./components/chat/Chat";
+import List from "./components/list/List";
+import Detail from "./components/detail/Detail";
+
+function App() {
+    return (
+        <div className="container">
+            <List />
+            <Chat />
+            <Detail />
+        </div>
+    );
+}
 
 export default App;
