791fc75a26bd448057a743e51e531a6c08ed23d3
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2024-05-27 21:54:22 +0530

    new-message section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,16 @@
 import "./chat.css";
+import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 
 export default function Chat() {
+    const [emojiOpen, setEmojiOpen] = useState(false);
+    const [message, setMessage] = useState("");
+
+    function handleEmoji(e) {
+        setMessage((prev) => prev + e.emoji);
+        setEmojiOpen(false);
+    }
+
     return (
         <div className="chat">
             <div className="chat-header">
@@ -20,7 +30,32 @@ export default function Chat() {
                 </div>
             </div>
             <div className="chat-messages"></div>
-            <div className="chat-new-message"></div>
+            <div className="chat-new-message">
+                <div className="new-message-icons">
+                    <img src="./img.png"></img>
+                    <img src="./camera.png"></img>
+                    <img src="./mic.png"></img>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Type a message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                ></input>
+                <div className="new-message-emoji">
+                    <img
+                        src="./emoji.png"
+                        onClick={() => setEmojiOpen((prev) => !prev)}
+                    ></img>
+                    <div className="emoji-picker">
+                        <EmojiPicker
+                            open={emojiOpen}
+                            onEmojiClick={handleEmoji}
+                        />
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
         </div>
     );
 }
