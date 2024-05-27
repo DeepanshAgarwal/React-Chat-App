d63514abaaa19ff44026f2bbbc96796c9e26a7bc
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2024-05-27 23:21:53 +0530

    chat-messages section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -29,7 +29,90 @@ export default function Chat() {
                     <img src="./info.png"></img>
                 </div>
             </div>
-            <div className="chat-messages"></div>
+            <div className="chat-messages">
+                <div className="chat-message">
+                    <img src="./avatar.png"></img>
+                    <div className="chat-message-text">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec nec eros eget sapien tempus aliquet.
+                            Duis at finibus odio. Sed a quam maximus,
+                            condimentum turpis ac, tempor magna.
+                        </p>
+                        <span className="chat-timestamp">1:30 PM</span>
+                    </div>
+                </div>
+                <div className="chat-message own-message">
+                    <div className="chat-message-text">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec nec eros eget sapien tempus aliquet.
+                            Duis at finibus odio. Sed a quam maximus,
+                            condimentum turpis ac, tempor magna.
+                        </p>
+                        <span className="chat-timestamp">1:30 PM</span>
+                    </div>
+                </div>
+                <div className="chat-message">
+                    <img src="./avatar.png"></img>
+                    <div className="chat-message-text">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec nec eros eget sapien tempus aliquet.
+                            Duis at finibus odio. Sed a quam maximus,
+                            condimentum turpis ac, tempor magna.
+                        </p>
+                        <span className="chat-timestamp">1:30 PM</span>
+                    </div>
+                </div>
+                <div className="chat-message own-message">
+                    <div className="chat-message-text">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec nec eros eget sapien tempus aliquet.
+                            Duis at finibus odio. Sed a quam maximus,
+                            condimentum turpis ac, tempor magna.
+                        </p>
+                        <span className="chat-timestamp">1:30 PM</span>
+                    </div>
+                </div>
+                <div className="chat-message">
+                    <img src="./avatar.png"></img>
+                    <div className="chat-message-text">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec nec eros eget sapien tempus aliquet.
+                            Duis at finibus odio. Sed a quam maximus,
+                            condimentum turpis ac, tempor magna.
+                        </p>
+                        <span className="chat-timestamp">1:30 PM</span>
+                    </div>
+                </div>
+                <div className="chat-message own-message">
+                    <div className="chat-message-text">
+                        <img src="https://images.unsplash.com/photo-1716718406268-6ece312abee0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec nec eros eget sapien tempus aliquet.
+                            Duis at finibus odio. Sed a quam maximus,
+                            condimentum turpis ac, tempor magna.
+                        </p>
+                        <span className="chat-timestamp">1:30 PM</span>
+                    </div>
+                </div>
+                {/* <div className="chat-message chat-receiver">
+                    <div className="chat-message-top">
+                        <span className="chat-message-name">John Doe</span>
+                        <span className="chat-message-timestamp">1:30 PM</span>
+                    </div>
+                    <div className="chat-message-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Donec nec eros eget sapien tempus aliquet. Duis at
+                        finibus odio. Sed a quam maximus, condimentum turpis ac,
+                        tempor magna.
+                    </div>
+                </div> */}
+            </div>
             <div className="chat-new-message">
                 <div className="new-message-icons">
                     <img src="./img.png"></img>
